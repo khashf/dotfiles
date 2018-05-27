56aa6b228c769a9ae25a72278c24da107bb81fac
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-05-26 23:01:59 -0700

    Source Intel C++ Compiler

--- bash/environment.bash
+++ bash/environment.bash
@@ -26,3 +26,6 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 export NVM_DIR="$HOME/.nvm";
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion";  # loads nvm bash_completion
+
+# Intel C++ compiler (icpc)
+source /opt/intel/parallel_studio_xe_2018/bin/psxevars.sh > /dev/null 2>&1
