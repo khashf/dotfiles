8bd57e7835fa7972420b25c9dc7b1079f546201f
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-05-26 23:01:38 -0700

    Add $JAVA_HOME

--- bash/environment.bash
+++ bash/environment.bash
@@ -17,6 +17,8 @@ export CHEATCOLORS=true;
 # Make python use UTF-8 encoding for output to stdin, stdout, stderr
 export PYTHONIOENCODING='UTF-8';
 
+export JAVA_HOME="/usr/"
+
 # colored GCC warnings and errors;
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01' 
 
